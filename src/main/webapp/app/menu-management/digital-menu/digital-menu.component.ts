6b80da522d0ef4f84f557b8ac15dd8686a9956e5
Aya <aya.mehry30@gmail.com>
2021-06-03 01:49:05 +0100

    update

--- src/main/webapp/app/menu-management/digital-menu/digital-menu.component.ts
+++ src/main/webapp/app/menu-management/digital-menu/digital-menu.component.ts
@@ -1,32 +1,182 @@
 import { Component, OnInit } from '@angular/core';
 import { DigitalMenu } from 'app/menu-management/digital-menu/digital-menu.model';
 import { DigitalMenuService } from 'app/menu-management/digital-menu/digital-menu.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+import { UploadedFile } from 'app/menu-management/menu-file/uploaded-file.model';
+import { Product } from 'app/product/product.model';
+import { ProductService } from 'app/product/product.service';
+import { Category } from 'app/category/category.model';
+import { CategoryService } from 'app/category/category.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'jhi-digital-menu',
   templateUrl: './digital-menu.component.html',
   styleUrls: ['./digital-menu.component.scss'],
+  providers: [NgbRatingConfig],
 })
 export class DigitalMenuComponent implements OnInit {
+  // public productForm: FormGroup;
+  public menuForm: FormGroup;
+  public id!: number;
+  isSaving = false;
+  public image!: UploadedFile;
+  public product!: Product;
+  public digitalMenu!: DigitalMenu;
   public digitalMenuList: DigitalMenu[] = [];
-  public digitalForms: Map<number, FormGroup> = new Map<number, FormGroup>();
+  public productList: Product[] = [];
+  public categoryList: Category[] = [];
 
-  constructor(private fb: FormBuilder, private digitalMenuService: DigitalMenuService) {
-    // this.createForm();
+  editForm = this.fb.group({
+    name: [''],
+  });
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private config: NgbRatingConfig,
+    private fb: FormBuilder,
+    private categoryService: CategoryService,
+    private digitalMenuService: DigitalMenuService,
+    private productService: ProductService
+  ) {
+    config.max = 5;
+    config.readonly = true;
+    // this.productForm = this.createForm();
+    this.menuForm = this.createMenuForm();
   }
 
   ngOnInit(): void {
-    this.digitalMenuService.getAllDigitalMenu().subscribe(value => {
-      this.digitalMenuList = value;
+    this.activatedRoute.params.subscribe(data => {
+      this.id = data['idEstab'];
+
+      this.digitalMenuService.getAlldigitalMenusByEstablishment(this.id).subscribe(value => {
+        this.digitalMenu = value;
+      });
+    });
+    this.digitalMenuService.getDigitalMenu(this.digitalMenu.id).subscribe(digitalMenu => {
+      this.digitalMenu = digitalMenu;
+      this.updateForm(digitalMenu);
     });
   }
-  /* private createForm(){
-    return this.fb.group({
-      category:[''],
-      image:[''],
-      logo:['']
 
+  createMenuForm(): FormGroup {
+    return this.fb.group({
+      name: ['', [Validators.required]],
     });
-} */
+  }
+
+  public addMenu(): void {
+    const digitalMenu: DigitalMenu = new DigitalMenu();
+    digitalMenu.name = this.menuForm.get('name')?.value;
+    this.digitalMenuService.addDigitalMenu(digitalMenu, this.id).subscribe(() => {
+      this.menuForm.reset();
+      this.digitalMenuService.getAlldigitalMenusByEstablishment(this.id).subscribe(value => {
+        this.digitalMenu = value;
+      });
+    });
+  }
+
+  public delete(id: any): void {
+    this.digitalMenuService.deleteDigitalMenu(id).subscribe(() => {
+      this.digitalMenuService.getAlldigitalMenusByEstablishment(this.id).subscribe(value => {
+        this.digitalMenu = value;
+      });
+    });
+  }
+
+  public updateForm(digitalMenu: DigitalMenu): void {
+    this.menuForm.patchValue({
+      name: digitalMenu.name,
+    });
+  }
+
+  private editMenu(digitalMenu: DigitalMenu): void {
+    digitalMenu.name = this.editForm.get(['name'])!.value;
+  }
+  save(): void {
+    this.isSaving = true;
+    this.editMenu(this.digitalMenu);
+    if (this.digitalMenu.id !== undefined) {
+      this.digitalMenuService.updateDigitalMenu(this.digitalMenu).subscribe(
+        () => this.onSaveSuccess(),
+        () => this.onSaveError()
+      );
+    }
+  }
+
+  private onSaveSuccess(): void {
+    this.isSaving = false;
+    this.digitalMenuService.getAlldigitalMenusByEstablishment(this.id).subscribe(value => {
+      this.digitalMenu = value;
+    });
+  }
+
+  private onSaveError(): void {
+    this.isSaving = false;
+  }
+
+  // this.digitalMenuService.getDigitalMenu(this.digitalMenu.id).subscribe(() =>{
+  //
+  // this.digitalMenuService.updateDigitalMenu(digitalMenu).subscribe(() =>{
+  //   this.digitalMenuService.getAlldigitalMenusByEstablishment(this.id).subscribe(value => {
+  //     this.digitalMenu = value;
+  //   });
+  // });
+  // });
+
+  // createForm(): FormGroup {
+  //   return this.fb.group({
+  //     name: ['', [Validators.required]],
+  //     description: ['', [Validators.required]],
+  //     price: ['', [Validators.required]],
+  //     rating: ['', [Validators.required]],
+  //     image: [null, [Validators.required]],
+  //   });
+  // }
+
+  // public add(): void {
+  //   const product: Product = new Product();
+  //   product.name = this.productForm.get('name')?.value;
+  //   product.description = this.productForm.get('description')?.value;
+  //   product.price = this.productForm.get('price')?.value;
+  //   product.rating = this.productForm.get('rating')?.value;
+  //   product.image = this.image;
+  //   this.productService.addProduct(product).subscribe(() => {
+  //     this.productForm.reset();
+  //     this.image = new UploadedFile();
+  //     this.productService.getAllProducts().subscribe(value => {
+  //       this.productList = value;
+  //     });
+  //   });
+  // }
+
+  public onUploadFile($event: any): void {
+    const formData = new FormData();
+    const file: File = $event.files[0];
+
+    formData.append('files', file, file.name);
+    this.productService.upload(formData).subscribe(image => {
+      this.image = image;
+    });
+  }
+
+  // public delete(id: any): void {
+  //   this.productService.deleteProduct(id).subscribe(() => {
+  //     this.digitalMenuService.getAllDigitalMenu().subscribe(value => {
+  //       this.digitalMenuList = value;
+  //     });
+  //   });
+  // }
+
+  // deleteFile(id: number): void {
+  //   this.productService.deleteFile(id).subscribe(() => {
+  //     this.product.image = new UploadedFile();
+  //     this.productForm.get('image')?.reset();
+  //   });
+  // }
+
+  public getImageUrl(image: UploadedFile): string {
+    return image.path;
+  }
 }
--- src/main/webapp/app/menu-management/digital-menu/digital-menu.service.ts
+++ src/main/webapp/app/menu-management/digital-menu/digital-menu.service.ts
@@ -2,30 +2,34 @@ import { Injectable } from '@angular/core';
 import { SERVER_API_URL } from '../../app.constants';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { DigitalMenu, IProduct } from './digital-menu.model';
+import { DigitalMenu } from './digital-menu.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DigitalMenuService {
-  private apiServerUrl = SERVER_API_URL + 'api/digital-menu';
+  private apiServerUrl = SERVER_API_URL + 'api/digitalmenu';
 
   constructor(private http: HttpClient) {}
 
-  public addDigitalMenu(digitalMenu: IProduct): Observable<IProduct> {
-    return this.http.post<IProduct>(this.apiServerUrl, digitalMenu);
+  public addDigitalMenu(digitalMenu: DigitalMenu, id: number): Observable<DigitalMenu> {
+    return this.http.post<DigitalMenu>(`${this.apiServerUrl}/add/${id}`, digitalMenu);
   }
 
-  public getDigitalMenu(id: number): Observable<IProduct> {
-    return this.http.get<IProduct>(`${this.apiServerUrl}/${id}`);
+  public getDigitalMenu(id: number): Observable<DigitalMenu> {
+    return this.http.get<DigitalMenu>(`${this.apiServerUrl}/getone/${id}`);
   }
 
-  public updateDigitalMenu(digitalMenu: IProduct): Observable<IProduct> {
-    return this.http.put<IProduct>(this.apiServerUrl, digitalMenu);
+  public getAlldigitalMenusByEstablishment(id: number): Observable<DigitalMenu> {
+    return this.http.get<DigitalMenu>(`${this.apiServerUrl}/establishment/${id}`);
+  }
+
+  public updateDigitalMenu(digitalMenu: DigitalMenu): Observable<DigitalMenu> {
+    return this.http.put<DigitalMenu>(this.apiServerUrl + '/update', digitalMenu);
   }
 
   public deleteDigitalMenu(id: number): Observable<{}> {
-    return this.http.delete(`${this.apiServerUrl}/${id}`);
+    return this.http.delete(`${this.apiServerUrl}/delete/${id}`);
   }
 
   public getAllDigitalMenu(): Observable<DigitalMenu[]> {
